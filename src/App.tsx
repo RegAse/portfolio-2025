9b2a9225d7cce6840073792de8aeadb0a5314444
Guðmundur Axel <gudmundur210@gmail.com>
2025-02-09 16:54:21 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+// import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+	// const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<div className="container">
+				{/* Photo of me */}
+
+				{/* Tabs: Full-Stack, Front-End, Back-End, Game Dev */}
+
+				{/* Front-End: TV Show Website,  */}
+
+				{/* Back-End: TV Show Website */}
+
+				{/* Game Dev:  */}
+
+
+				{/* Tab Content grid of projects possibly gifs when hovered over or just thumbnails and able to enlarge it if clicked....*/}
+				<div className="row">
+					<div className="col-sm-3">
+						Hello
+					</div>
+					<div className="col-sm-3">
+						Hello
+					</div>
+					<div className="col-sm-3">
+						Hello
+					</div>
+					<div className="col-sm-3">
+						Hello
+					</div>
+					<div className="col-sm-3">
+						Hello
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
