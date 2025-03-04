46732c0a7df599bab8ec68be03c1f9eee0ccdb95
Guðmundur Axel <gudmundur210@gmail.com>
2025-03-04 15:44:07 +0000

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:
-  {
-    outDir: './build',
-    emptyOutDir: true
-  }
+  base: "https://regase.github.io/Portfolio-2025/"
 })
