4e6290b9620128414c928d6e199bc648c9b7ece7
Guðmundur Axel <gudmundur210@gmail.com>
2025-03-05 16:37:38 +0000

    Fixed some minor responsive issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,11 +26,11 @@ function Home() {
 				<div className="container">
 					<div className="profile-section">
 					<div className="row">
-						<div className="col-md-2">
+						<div className="col-6 col-sm-6 col-md-2">
 							<img className="img-fluid mx-auto" height={150} src="/portfolio-2025/images/me-4.jpg" alt="Failed" />
 						</div>
-						<div className="col-md-9">
-							<h2>Guðmundur Axel Guðrúnarson</h2>
+						<div className="col-9 col-md-9">
+							<h2 className="mt-3">Guðmundur Axel Guðrúnarson</h2>
 							<p>I work hard and one of my best skills is to always keep learning and adapting to tech and 
 								thus I am very comfortable with working on various languages or frameworks. And not only limited 
 								to the ones that I am already experienced in such as React, TypeScript, SCSS (CSS), Node.js, Next.js,
@@ -47,10 +47,10 @@ function Home() {
 				<div className="tab-section">
 					<div className="row">
 						<ul className="tab-links">
-							<li className="col-sm-3"><Link to={"/"} className={(stack == "" || stack == undefined ? "active" : "")}>Highlighted</Link></li>
-							<li className="col-sm-3"><Link to={"/stack/front-end"} className={(stack == "front-end" ? "active" : "")}>Front-End</Link></li>
-							<li className="col-sm-3"><Link to={"/stack/back-end"} className={(stack == "back-end" ? "active" : "")}>Back-End</Link></li>
-							<li className="col-sm-3"><Link to={"/stack/game-dev"} className={(stack == "game-dev" ? "active" : "")}>Game Dev</Link></li>
+							<li className="col-6 col-sm-3"><Link to={"/"} className={(stack == "" || stack == undefined ? "active" : "")}>Highlighted</Link></li>
+							<li className="col-6 col-sm-3"><Link to={"/stack/front-end"} className={(stack == "front-end" ? "active" : "")}>Front-End</Link></li>
+							<li className="col-6 col-sm-3"><Link to={"/stack/back-end"} className={(stack == "back-end" ? "active" : "")}>Back-End</Link></li>
+							<li className="col-6 col-sm-3"><Link to={"/stack/game-dev"} className={(stack == "game-dev" ? "active" : "")}>Game Dev</Link></li>
 						</ul>
 					</div>
 				</div>
@@ -67,7 +67,7 @@ function Home() {
 					<h6 className="mb-3 mt-3">Projects</h6>
 					<div className="row gx-3 gy-3">
 						{projects.map((project) => (
-							<div className="col-sm-3">
+							<div className="col-12 col-sm-6 col-md-4 col-lg-3">
 								<div className="project-card" 
 									style={{ 
 										backgroundImage: `url(${project.cover})`,
